09d69167f5d57d954ee319e98be0db7bff3a0f37
boeyjunhui <boeyjunhui@localhost>
2023-03-26 11:39:15 +0800

    insignificant data v1

---
+++ src/model/AdminAuthRecord.java
@@ -0,0 +1,5 @@
+package model;
+
+public record AdminAuthRecord(int adminID, boolean auth) {
+    
+}
---
+++ src/model/ProductRecord.java
@@ -0,0 +1,5 @@
+package model;
+
+public record ProductRecord(String productUniqueCode, String productName, String productDescription, String ingredient, String nutrition, String weight, double price, String expiryDate) {
+    
+}
